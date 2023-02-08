942edc43550e2ab053fc1ce5fb31478c10ba7847
Bjørge Næss <bjoerge@gmail.com>
2023-02-08 02:46:31 +0100

    remove deprecated factories

--- src/shapeDefs.ts
+++ src/shapeDefs.ts
@@ -1,29 +1,62 @@
-import {_object, boolean, literal, optional, string,} from "./__deprecated_factories.js"
-import {Conceal, Infer, OutputFromShape, SanityLiteral, SanityString, SanityType,} from "./defs.js"
-import {Combine, OutputFormatFix} from "./utils.js"
+import {
+  Conceal,
+  Infer,
+  OutputFromShape,
+  SanityBoolean,
+  SanityLiteral,
+  SanityObject,
+  SanityOptional,
+  SanityString,
+  SanityType,
+} from "./defs.js"
+import {Combine, defineNonEnumerableGetter, OutputFormatFix} from "./utils.js"
 
-export const documentBase = _object({
-  _type: string(),
-  _id: string(),
-  _createdAt: string(),
-  _updatedAt: string(),
-  _rev: string(),
+const STRING: SanityString = dt({typeName: "string", def: ""})
+const BOOLEAN: SanityBoolean = dt({
+  typeName: "boolean",
+  def: true,
 })
-
-export const referenceBase = _object({
-  _type: literal("reference"),
-  _ref: string(),
-  _weak: optional(boolean()),
-})
-export type ReferenceBase = Infer<typeof referenceBase>
 
 export type SanityDocumentShape = {
-  _type: SanityLiteral<string>
+  _type: SanityString | SanityLiteral<string>
   _id: SanityString
   _createdAt: SanityString
   _updatedAt: SanityString
   _rev: SanityString
 }
+
+export const documentBase: SanityObject<SanityDocumentShape> = dt({
+  typeName: "object",
+  def: {
+    _type: STRING,
+    _id: STRING,
+    _createdAt: STRING,
+    _updatedAt: STRING,
+    _rev: STRING,
+  },
+})
+
+export type SanityReferenceShape = {
+  _type: SanityString | SanityLiteral<"reference">
+  _ref: SanityString
+  _weak: SanityOptional<SanityBoolean>
+}
+
+const REFERENCE_LITERAL: SanityLiteral<"reference"> = dt({
+  typeName: "literal",
+  def: "reference",
+})
+
+export const referenceBase: SanityObject<SanityReferenceShape> = dt({
+  typeName: "object",
+  def: {
+    _type: REFERENCE_LITERAL,
+    _ref: STRING,
+    _weak: dt({typeName: "optional" as const, def: BOOLEAN}),
+  },
+})
+
+export type ReferenceBase = Infer<typeof referenceBase>
 
 export type SanityArrayValue<ElementType> = Array<
   ElementType extends object
@@ -38,3 +71,13 @@ export type SanityReferenceValue<RefType> = Combine<
   ReferenceBase,
   Conceal<SanityType<RefType>>
 >
+
+/**
+ * defineType
+ * @param target
+ */
+function dt<T>(target: T): T & {output: never} {
+  return defineNonEnumerableGetter(target, "output", () => {
+    throw new Error("This method is not defined runtime")
+  }) as T & {output: never}
+}
