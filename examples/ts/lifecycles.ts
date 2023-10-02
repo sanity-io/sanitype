5c1cfa73d91f42b701303e74c3d10ab5c66d8ba9
Bjørge Næss <bjoerge@gmail.com>
2023-10-02 23:02:33 +0200

    feat: implement lifecycle helpers: draft, stored

---
+++ examples/ts/lifecycles.ts
@@ -0,0 +1,30 @@
+import {
+  boolean,
+  document,
+  literal,
+  number,
+  object,
+  optional,
+  parse,
+  string,
+  union,
+} from 'sanitype'
+import {draft, stored} from '../../src/lifecycle'
+
+const doc = document({
+  _type: literal('pet'),
+  name: string(),
+  union: union([string(), number()]),
+  opt: optional(boolean()),
+  nested: object({
+    foo: optional(string()),
+  }),
+})
+
+const storedDoc = parse(stored(doc), {})
+
+const draftDoc = draft(doc)
+
+const parsedDraft = parse(draftDoc, {})
+
+const parsedStoredDraft = parse(stored(draftDoc), {})
---
+++ src/lifecycle.ts
@@ -0,0 +1,42 @@
+import {deepPartial} from './utils/deepPartial'
+import {document} from './creators'
+import {required} from './utils/required'
+import type {DeepPartial} from './utils/deepPartial'
+import type {ArrayElement} from '@bjoerge/mutiny'
+import type {SanityDocument} from './defs'
+import type {RequiredShape} from './utils/shallowRequired'
+
+const STORED_KEYS = [
+  '_type',
+  '_id',
+  '_createdAt',
+  '_updatedAt',
+  '_rev',
+] as const
+type StoredKeys = ArrayElement<typeof STORED_KEYS>
+
+type Stored<T extends SanityDocument<any>> = T extends SanityDocument<
+  infer DocShape
+>
+  ? SanityDocument<
+      RequiredShape<Pick<DocShape, StoredKeys>> & Omit<DocShape, StoredKeys>
+    >
+  : never
+
+type Draft<T extends SanityDocument> = DeepPartial<T>
+
+export function draft<T extends SanityDocument>(schema: T): Draft<T> {
+  return deepPartial(schema)
+}
+
+export function stored<T extends SanityDocument>(schema: T): Stored<T> {
+  const storedShape = Object.entries(schema.shape).map(([key, propSchema]) => {
+    return [
+      key,
+      STORED_KEYS.includes(key as StoredKeys)
+        ? required(propSchema)
+        : propSchema,
+    ]
+  })
+  return document(Object.fromEntries(storedShape)) as any
+}
