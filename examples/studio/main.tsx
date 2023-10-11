db6bed9d0d7bbe405bb86993f231de2b0c5cf044
Bjørge Næss <bjoerge@gmail.com>
2023-10-11 22:14:27 +0200

    chore(deps): remove unused refractor usage

--- examples/studio/main.tsx
+++ examples/studio/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Refractor from 'react-refractor'
-import json from 'refractor/lang/json'
-
 import {Root} from './Root'
 import App from './App'
 
-Refractor.registerLanguage(json)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Root>
