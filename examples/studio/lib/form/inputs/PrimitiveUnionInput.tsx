594a5bde4e21dfcfb90132b5b977b0c51c1b505d
Bjørge Næss <bjoerge@gmail.com>
2023-10-11 22:40:11 +0200

    fix: improve primitive union input

--- examples/studio/lib/form/inputs/PrimitiveUnionInput.tsx
+++ examples/studio/lib/form/inputs/PrimitiveUnionInput.tsx
@@ -1,8 +1,7 @@
-import {Box, Button, Flex, Select, Stack} from '@sanity/ui'
+import {Box, Flex, Select, Stack} from '@sanity/ui'
 import {useCallback} from 'react'
 import {at, set, unset} from '@bjoerge/mutiny'
 import {isLiteralSchema} from 'sanitype'
-import {CloseIcon} from '@sanity/icons'
 import type {SanityPrimitiveUnion} from 'sanitype'
 import type {InputProps} from '../types'
 
@@ -10,19 +9,12 @@ export function PrimitiveUnionInput(props: InputProps<SanityPrimitiveUnion>) {
   const {value} = props
 
   const handleReplaceType = useCallback(
-    (nextValue: string) => {
+    (nextValue: string) =>
       props.onPatch({
-        patches: [at([], set(nextValue))],
-      })
-    },
+        patches: [at([], nextValue === '' ? unset() : set(nextValue))],
+      }),
     [props.onPatch],
   )
-
-  const handleClear = useCallback(() => {
-    props.onPatch({
-      patches: [at([], unset())],
-    })
-  }, [props.onPatch])
 
   const literalTypes = props.schema.union.filter(isLiteralSchema)
   // todo: support non-literal primitives
@@ -34,7 +26,7 @@ export function PrimitiveUnionInput(props: InputProps<SanityPrimitiveUnion>) {
           space={[3, 3, 4]}
           onChange={e => handleReplaceType(e.currentTarget.value)}
         >
-          {value ? null : <option value="">Select…</option>}
+          <option value="">{value ? '' : 'Select…'}</option>
           {literalTypes.map((ut, i) => {
             const literalValue = String(ut.value)
             return (
@@ -44,21 +36,8 @@ export function PrimitiveUnionInput(props: InputProps<SanityPrimitiveUnion>) {
             )
           })}
         </Select>
-        {value ? (
-          <Button
-            mode="bleed"
-            tone="critical"
-            icon={CloseIcon}
-            title="Clear"
-            onClick={handleClear}
-          />
-        ) : null}
       </Flex>
       <Box></Box>
     </Stack>
   )
-}
-
-function intersection<T1>(a: T1[], b: T1[]): T1[] {
-  return a.filter(x => b.includes(x))
 }
