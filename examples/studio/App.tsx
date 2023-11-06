4196a04a5f74bb56a87f752ab543360da7d6d4f5
Bjørge Næss <bjoerge@gmail.com>
2023-11-06 02:15:17 +0100

    sort out connection issues

--- examples/studio/App.tsx
+++ examples/studio/App.tsx
@@ -15,7 +15,9 @@ import {
 } from '@sanity/ui'
 import {
   ReplaySubject,
+  defer,
   filter,
+  finalize,
   map,
   merge,
   mergeMap,
@@ -122,20 +124,18 @@ let _globalListener: {
 function shareReplayDelayedDisconnect<T>(
   delay: number,
 ): MonoTypeOperatorFunction<T> {
-  const bufferSize = 1
-  const refCount = true
   return share<T>({
-    connector: () => new ReplaySubject(bufferSize, Infinity),
+    connector: () => new ReplaySubject(1, Infinity),
     resetOnError: true,
-    resetOnComplete: false,
+    resetOnComplete: true,
     resetOnRefCountZero: () => timer(delay),
   })
 }
 
 const getGlobalEvents = () => {
   if (!_globalListener) {
-    const allEvents$ = client
-      .listen(
+    const allEvents$ = defer(() =>
+      client.listen(
         '*[!(_id in path("_.**"))]',
         {},
         {
@@ -144,15 +144,15 @@ const getGlobalEvents = () => {
           visibility: 'query',
           effectFormat: 'mendoza',
         },
-      )
-      .pipe(shareReplayDelayedDisconnect(1000))
+      ),
+    ).pipe(shareReplayDelayedDisconnect(1000))
 
     // This is a stream of welcome events from the server, each telling us that we have established listener connection
     // We map these to snapshot fetch/sync. It is good to wait for the first welcome event before fetching any snapshots as, we may miss
     // events that happens in the time period after initial fetch and before the listener is established.
     const welcome$ = allEvents$.pipe(
       filter((event: any) => event.type === 'welcome'),
-      shareReplay({refCount: true, bufferSize: 1}),
+      shareReplayDelayedDisconnect(1000),
     )
 
     const mutations$ = allEvents$.pipe(
