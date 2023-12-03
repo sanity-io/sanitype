0c5422c0434f7f12593cacca476c886057ba6bdf
Bjørge Næss <bjoerge@gmail.com>
2023-12-03 01:46:08 +0000

    {wip} saved 2023-12-3 01:46

--- examples/studio/lib/form/inputs/DocumentInput.tsx
+++ examples/studio/lib/form/inputs/DocumentInput.tsx
@@ -1,13 +1,20 @@
 import {Stack, Text} from '@sanity/ui'
 import {at, patch} from '@bjoerge/mutiny'
-import {useCallback} from 'react'
-import type {SanityDocument} from 'sanitype'
-import type {DocumentInputProps, PatchEvent} from '../types'
+import {memo, useCallback, useMemo} from 'react'
+import type {
+  CommonFieldOptions,
+  Infer,
+  SanityAny,
+  SanityFormDef,
+} from 'sanitype'
+import type {ComponentType} from 'react'
+import type {SanityDocument, SanityType} from 'sanitype'
+import type {DocumentInputProps, InputProps, PatchEvent} from '../types'
 
 export function DocumentInput<T extends SanityDocument>(
   props: DocumentInputProps<T>,
 ) {
-  const {value, onMutation, schema} = props
+  const {value, onMutation, resolveInput} = props
   const handleFieldPatch = useCallback(
     (fieldName: string, patchEvent: PatchEvent) => {
       onMutation({
@@ -21,26 +28,56 @@ export function DocumentInput<T extends SanityDocument>(
   return (
     <Stack space={4}>
       {Object.entries(props.form.fields).map(([fieldName, fieldOptions]) => {
-        const fieldSchema = props.schema.shape[fieldName]
-        const fieldValue = value?.[fieldName]
-        const Input = props.resolveInput(fieldSchema)
         return (
-          <Stack key={fieldName} space={3}>
-            <label>
-              <Text size={1} weight="semibold">
-                {fieldOptions.title}
-              </Text>
-            </label>
-            <Input
-              form={fieldOptions.form}
-              schema={fieldSchema}
-              onPatch={patchEvent => handleFieldPatch(fieldName, patchEvent)}
-              value={fieldValue}
-              resolveInput={props.resolveInput}
-            />
-          </Stack>
+          <Field
+            resolveInput={resolveInput}
+            key={fieldName}
+            schema={props.schema.shape[fieldName]}
+            value={value?.[fieldName]}
+            onPatch={handleFieldPatch}
+            options={fieldOptions}
+            name={fieldName}
+          />
         )
       })}
     </Stack>
   )
 }
+
+export type FieldProps<Schema extends SanityAny> = {
+  schema: Schema
+  name: string
+  value?: Infer<Schema>
+  options: CommonFieldOptions
+  onPatch: (fieldName: string, patchEvent: PatchEvent) => void
+  // onMutate: (mutationEvent: MutationEvent) => void // todo: consider support for patching other documents too
+  resolveInput: <T extends SanityAny>(schema: T) => ComponentType<InputProps<T>>
+}
+
+const Field = memo(function Field<Schema extends SanityType>(
+  props: FieldProps<Schema>,
+) {
+  const {schema, name, value, resolveInput, onPatch, options} = props
+
+  const handlePatch = useCallback(
+    (patchEvent: PatchEvent) => onPatch(name, patchEvent),
+    [name, onPatch],
+  )
+  const Input = useMemo(() => resolveInput(schema), [resolveInput, schema])
+  return (
+    <Stack space={3}>
+      <label>
+        <Text size={1} weight="semibold">
+          {options.title}
+        </Text>
+      </label>
+      <Input
+        schema={schema}
+        onPatch={handlePatch}
+        value={value}
+        resolveInput={resolveInput}
+        form={options.form as SanityFormDef<Schema>}
+      />
+    </Stack>
+  )
+})
