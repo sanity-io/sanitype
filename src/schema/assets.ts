8de5f543444f4524e0f5cc3fc8911871f010a702
Ash <ash@sanity.io>
2023-12-06 14:30:40 +0000

    feat: add system asset schemas

---
+++ src/schema/assets.ts
@@ -0,0 +1,74 @@
+import {boolean, literal, number, object, optional, string} from '../creators'
+import {extend} from '../utils/extend'
+
+export const assetSourceSpec = object({
+  id: string(),
+  name: string(),
+  url: optional(string()),
+})
+
+export const asset = object({
+  url: string(),
+  path: string(),
+  assetId: string(),
+  extension: string(),
+  mimeType: string(),
+  sha1hash: string(),
+  size: number(),
+  originalFilename: optional(string()),
+
+  // Extensions
+  label: optional(string()),
+  title: optional(string()),
+  description: optional(string()),
+
+  // External asset source extensions
+  creditLine: optional(string()),
+  source: optional(assetSourceSpec),
+})
+
+export const imageDimensions = object({
+  _type: literal('sanity.imageDimensions'),
+  height: number(),
+  width: number(),
+  aspectRatio: number(),
+})
+
+export const imageSwatch = object({
+  _type: literal('sanity.imagePaletteSwatch'),
+  background: string(),
+  foreground: string(),
+  population: number(),
+  title: optional(string()),
+})
+
+export const imagePalette = object({
+  _type: literal('sanity.imagePalette'),
+  darkMuted: optional(imageSwatch),
+  darkVibrant: optional(imageSwatch),
+  dominant: optional(imageSwatch),
+  lightMuted: optional(imageSwatch),
+  lightVibrant: optional(imageSwatch),
+  muted: optional(imageSwatch),
+  vibrant: optional(imageSwatch),
+})
+
+export const imageMetadata = object({
+  _type: literal('sanity.imageMetadata'),
+  dimensions: imageDimensions,
+  palette: optional(imagePalette),
+  lqip: optional(string()),
+  blurHash: optional(string()),
+  hasAlpha: boolean(),
+  isOpaque: boolean(),
+})
+
+export const imageAsset = extend(asset, {
+  _type: literal('sanity.imageAsset'),
+  metadata: imageMetadata,
+})
+
+export const fileAsset = extend(asset, {
+  _type: literal('sanity.fileAsset'),
+  metadata: imageMetadata,
+})
