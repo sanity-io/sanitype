b5814a217dc4266a401f99eee4a739c9acb67cda
Bjørge Næss <bjoerge@gmail.com>
2023-12-06 12:20:43 +0000

    fix(test): exclude .tmp from vitest typechecks

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import {configDefaults, defineConfig} from 'vitest/config'
 
 export default defineConfig({
   test: {
+    typecheck: {
+      exclude: [...(configDefaults.typecheck?.exclude || []), '.tmp/**'],
+    },
     exclude: [...configDefaults.exclude, '.tmp/**'],
-    includeSource: ['src/**/*.ts'],
+    includeSource: ['./src/**/*.ts'],
   },
 })
