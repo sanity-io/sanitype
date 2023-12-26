4f8dba726c9c828a9d9195ef61d2188c9339cf8b
Bjørge Næss <bjoerge@gmail.com>
2023-12-26 16:34:35 +0100

    refactor(test): extract schema fixtures

---
+++ src/compat/typed-to-classic/__test__/schemas/classic.ts
@@ -0,0 +1,147 @@
+export const classicHuman = [
+  {
+    name: 'human',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+      },
+      {
+        name: 'address',
+        type: 'object',
+        fields: [
+          {
+            name: 'street',
+            type: 'string',
+          },
+          {
+            name: 'city',
+            type: 'string',
+          },
+          {
+            name: 'country',
+            type: 'string',
+          },
+        ],
+      },
+      {
+        name: 'profilePicture',
+        type: 'image',
+        fields: [
+          {
+            name: 'alt',
+            type: 'string',
+          },
+        ],
+      },
+      {
+        name: 'cv',
+        type: 'file',
+        fields: [
+          {
+            name: 'description',
+            type: 'string',
+          },
+        ],
+      },
+      {
+        name: 'portableText',
+        type: 'array',
+        of: [
+          {
+            type: 'block',
+            name: 'myCustomBlock',
+            of: [
+              {
+                fields: [
+                  {
+                    name: 'name',
+                    type: 'string',
+                  },
+                ],
+                name: 'author',
+                type: 'object',
+              },
+              {
+                fields: [
+                  {
+                    name: 'marks',
+                    of: [
+                      {
+                        options: {
+                          list: ['strong'],
+                        },
+                        type: 'string',
+                      },
+                    ],
+                    type: 'array',
+                  },
+                  {
+                    name: 'text',
+                    type: 'string',
+                  },
+                ],
+                name: 'span',
+                type: 'object',
+              },
+            ],
+          },
+          {
+            name: 'image',
+            type: 'image',
+            fields: [],
+          },
+        ],
+      },
+      {
+        name: 'pets',
+        type: 'array',
+        of: [
+          {
+            name: 'avine',
+            type: 'object',
+            fields: [
+              {
+                name: 'name',
+                type: 'string',
+              },
+              {
+                name: 'squeaks',
+                type: 'boolean',
+              },
+            ],
+          },
+          {
+            name: 'feline',
+            type: 'object',
+            fields: [
+              {
+                name: 'name',
+                type: 'string',
+              },
+              {
+                name: 'meows',
+                type: 'boolean',
+              },
+            ],
+          },
+          {
+            name: 'canine',
+            type: 'object',
+            fields: [
+              {
+                name: 'name',
+                type: 'string',
+              },
+              {
+                name: 'barks',
+                type: 'boolean',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
---
+++ src/compat/typed-to-classic/__test__/schemas/typed.ts
@@ -0,0 +1,68 @@
+import {
+  array,
+  block,
+  boolean,
+  document,
+  file,
+  image,
+  literal,
+  number,
+  object,
+  string,
+  union,
+} from '../../../../creators'
+import {extend} from '../../../../utils/extend'
+
+const pet = object({name: string()})
+export const human = document({
+  _type: literal('human'),
+  name: string(),
+  address: object({
+    street: string(),
+    city: string(),
+    country: string(),
+  }),
+  profilePicture: image({
+    alt: string(),
+  }),
+  cv: file({
+    description: string(),
+  }),
+  portableText: array(
+    union([
+      block({
+        _type: literal('myCustomBlock'),
+        styles: [literal('normal'), literal('h1'), literal('h2')],
+        lists: [literal('bullet'), literal('number')],
+        inlineTypes: [
+          object({
+            _type: literal('author'),
+            name: string(),
+          }),
+        ],
+        decorators: [literal('strong'), literal('em')],
+        annotations: [
+          object({_type: literal('author'), foo: number()}),
+          object({_type: literal('book'), bar: number()}),
+        ],
+      }),
+      image({}),
+    ]),
+  ),
+  pets: array(
+    union([
+      extend(pet, {
+        _type: literal('avine'),
+        squeaks: boolean(),
+      }),
+      extend(pet, {
+        _type: literal('feline'),
+        meows: boolean(),
+      }),
+      extend(pet, {
+        _type: literal('canine'),
+        barks: boolean(),
+      }),
+    ]),
+  ),
+})
