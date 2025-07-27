ad72f7e9d266ac72cc685b854e88886a02ee3413
Bjørge Næss <bjoerge@gmail.com>
2025-07-27 23:05:36 +0200

    feat: add safeint, uint32, int32, float32, float64

--- src/creators/number.ts
+++ src/creators/number.ts
@@ -4,3 +4,23 @@ import {defineType} from '../helpers/defineType'
 export function number(constraints?: NumberConstraints): SanityNumber {
   return defineType({typeName: 'number', constraints})
 }
+
+export function safeint() {
+  return number({min: Number.MIN_SAFE_INTEGER, max: Number.MAX_SAFE_INTEGER})
+}
+
+export function uint32() {
+  return number({min: 0, max: 4294967295})
+}
+
+export function int32() {
+  return number({min: -2147483648, max: 2147483647})
+}
+
+export function float32() {
+  return number({min: -3.4028234663852886e38, max: 3.4028234663852886e38})
+}
+
+export function float64() {
+  return number({min: -Number.MAX_VALUE, max: Number.MAX_VALUE})
+}
